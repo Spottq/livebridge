0bc8d454616a49f72a6ee77c9624c7087f155b1a
Spottq <spottq@users.noreply.github.com>
2026-04-08 18:15:29 +0300

    Change app id to KimGiSa

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.nhn.android.search"
+        applicationId = "com.locnall.KimGiSa"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // Live Updates are available starting from Android 16 (API 36).
