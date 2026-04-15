5af0a9075475d2170160dd4e3dbc0977bfb369e3
Spottq <spottq@users.noreply.github.com>
2026-04-15 22:20:50 +0300

    Revert "Use nowbar-sdk for collapsed flashlight now bar"
    This reverts commit 66f65698b46c948734f1779b0d3d913e162becd9.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,12 +11,12 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
@@ -45,7 +45,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("io.github.d4viddf:hyperisland_kit:0.4.3")
-    implementation(project(":nowbar"))
 }
 
 flutter {
--- android/nowbar/src/main/kotlin/com/nowbar/api/notification/ProgressSegment.kt
+++
@@ -1,9 +0,0 @@
-package com.nowbar.api.notification
-
-import android.graphics.drawable.Icon
-
-data class ProgressSegment(
-    val startPosition: Float,
-    val color: Int,
-    val icon: Icon? = null
-)
